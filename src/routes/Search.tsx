89511124b5fa88c22fc9b1da3fa0f0fe8b1694cf
ashirbad behera <ashirbad.behera@squareboat.com>
2022-05-01 23:53:20 +0530

    minor isssue fixes

--- src/routes/Search.tsx
+++ src/routes/Search.tsx
@@ -21,7 +21,7 @@ const Search = () => {
     <main className="flex">
       <div className="container h-full">
         <Navbar onSearchInputChange={(val) => debouncedSetQuery(val)} />
-        <section className="flex-1 flex flex-col overflow-y-auto overflow-x-scroll">
+        <section className="flex-1 flex flex-col overflow-y-auto scroll-bar">
           {isLoading ? (
             <div className="flex flex-1 items-center justify-center">Loading...</div>
           ) : (
