dca6fb4b9dd5e4450b360ce31d0f90a44766d90c
ashirbad behera <ashirbad.behera@squareboat.com>
2022-05-02 07:39:09 +0530

    fix

--- src/routes/Popular.tsx
+++ src/routes/Popular.tsx
@@ -11,6 +11,8 @@ export type ParamsType = {
   with_genres?: string;
   'primary_release_date.gte'?: string;
   'primary_release_date.lte'?: string;
+  'first_air_date.gte'?: string;
+  'first_air_date.lte'?: string;
   'vote_average.lte'?: number;
 };
 
@@ -54,8 +56,14 @@ const Popular = () => {
           type: showType,
           genre: params.with_genres,
           year: {
-            start: params['primary_release_date.gte'],
-            end: params['primary_release_date.lte'],
+            start:
+              showType === 'movie'
+                ? params['primary_release_date.gte']
+                : params['first_air_date.gte'],
+            end:
+              showType === 'movie'
+                ? params['primary_release_date.lte']
+                : params['first_air_date.lte'],
           },
           rating: Math.round((params['vote_average.lte'] || 0) / 2),
         }}
@@ -64,14 +72,39 @@ const Popular = () => {
         }
         onTypeChange={(type) => {
           setShowType((show) => (type === 'movie' || type === 'tv' ? type : show));
-          setParams((params) => ({ ...params, page: 1 }));
+          const {
+            'first_air_date.gte': _,
+            'first_air_date.lte': __,
+            'primary_release_date.gte': ___,
+            'primary_release_date.lte': ____,
+            ...rest
+          } = params;
+          setParams({ ...rest, page: 1 });
         }}
-        onFromYearChange={(year) =>
-          setParams({ ...params, 'primary_release_date.gte': `${year}-01-01`, page: 1 })
-        }
-        onToYearChange={(year) =>
-          setParams({ ...params, 'primary_release_date.lte': `${year}-12-31`, page: 1 })
-        }
+        onFromYearChange={(year) => {
+          const { 'first_air_date.gte': _, 'primary_release_date.gte': __ } = params;
+          if (showType === 'tv') {
+            setParams({ ...params, 'first_air_date.gte': `${year}-01-01`, page: 1 });
+          } else {
+            setParams({
+              ...params,
+              'primary_release_date.gte': `${year}-01-01`,
+              page: 1,
+            });
+          }
+        }}
+        onToYearChange={(year) => {
+          const { 'first_air_date.lte': _, 'primary_release_date.lte': __ } = params;
+          if (showType === 'tv') {
+            setParams({ ...params, 'first_air_date.lte': `${year}-12-31`, page: 1 });
+          } else {
+            setParams({
+              ...params,
+              'primary_release_date.lte': `${year}-12-31`,
+              page: 1,
+            });
+          }
+        }}
         onRatingChange={(val) =>
           setParams((params) => ({ ...params, 'vote_average.lte': val, page: 1 }))
         }
